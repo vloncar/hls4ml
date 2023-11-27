0c07bde369d432a6dde46bb4ec45bdad876d1509
Vladimir Loncar <vloncar@users.noreply.github.com>
2023-11-27 20:16:27 +0100

    Change interface of Gather op (yet to be optmized)

--- hls4ml/backends/vivado/passes/gather_templates.py
+++ hls4ml/backends/vivado/passes/gather_templates.py
@@ -41,12 +41,29 @@ class GetItemFunctionTemplate(FunctionCallTemplate):
 
 # Gather templates
 
-gather_config_template = """struct config{index} : nnet::gather_config {{
+gather_1d_config_template = """struct config{index} : nnet::gather_config_1d {{
     static const unsigned n_in = {n_in};
+    static const unsigned n_indices = {n_indices};
+    static const unsigned n_out = n_indices;
+}};\n"""
+
+gather_2d_config_template = """struct config{index} : nnet::gather_config_2d {{
+    static const unsigned n_in_0 = {n_in_0};
+    static const unsigned n_in_1 = {n_in_1};
     static const unsigned n_indices = {n_indices};
 }};\n"""
 
-gather_function_template = 'nnet::gather<{input_t}, {index_t}, {config}>({input}, {indices}, {output});'
+
+gather_3d_config_template = """struct config{index} : nnet::gather_config_3d {{
+    static const unsigned n_in_0 = {n_in_0};
+    static const unsigned n_in_1 = {n_in_1};
+    static const unsigned n_in_2 = {n_in_2};
+    static const unsigned n_indices = {n_indices};
+    
+}};\n"""
+
+
+gather_function_template = 'nnet::gather_{rank}d<{input_t}, {index_t}, {config}>({input}, {indices}, {output});'
 
 gather_include_list = ['nnet_utils/nnet_gather.h']
 
@@ -54,14 +71,36 @@ gather_include_list = ['nnet_utils/nnet_gather.h']
 class GatherConfigTemplate(LayerConfigTemplate):
     def __init__(self):
         super().__init__(Gather)
-        self.template = gather_config_template
+        self.templates = {
+            1: gather_1d_config_template,
+            2: gather_2d_config_template,
+            3: gather_3d_config_template,
+        }
 
     def format(self, node):
         params = self._default_config_params(node)
         params['n_in'] = np.prod(node.get_input_variable(node.inputs[0]).shape[1:])
+
         params['n_indices'] = node.get_input_variable(node.inputs[1]).shape[0]
 
-        return self.template.format(**params)
+        rank = len(node.get_input_variable(node.inputs[0]).shape)
+        if rank == 1:
+            params['n_in'] = node.get_input_variable(node.inputs[0]).size()
+
+            return self.templates[1].format(**params)
+        elif rank == 2:
+            params['n_in_0'] = node.get_input_variable(node.inputs[0]).shape[0]
+            params['n_in_1'] = node.get_input_variable(node.inputs[0]).shape[1]
+            
+            return self.templates[2].format(**params)
+        elif rank == 3:
+            params['n_in_0'] = node.get_input_variable(node.inputs[0]).shape[0]
+            params['n_in_1'] = node.get_input_variable(node.inputs[0]).shape[1]
+            params['n_in_2'] = node.get_input_variable(node.inputs[0]).shape[2]
+
+            return self.templates[3].format(**params)
+        else:
+            return None
 
 
 class GatherFunctionTemplate(FunctionCallTemplate):
@@ -73,5 +112,7 @@ class GatherFunctionTemplate(FunctionCallTemplate):
         params = self._default_function_params(node)
         params['index_t'] = node.get_input_variable(node.inputs[1]).type.name
         params['indices'] = node.get_input_variable(node.inputs[1]).name
+
+        params['rank'] = len(node.get_input_variable(node.inputs[0]).shape)
 
         return self.template.format(**params)
--- hls4ml/templates/vivado/nnet_utils/nnet_gather.h
+++ hls4ml/templates/vivado/nnet_utils/nnet_gather.h
@@ -22,18 +22,59 @@ template <class data_T, typename CONFIG_T> void getitem(data_T input[CONFIG_T::n
     }
 }
 
-struct gather_config {
+struct gather_config_1d {
     static const unsigned n_in = 20;
     static const unsigned n_indices = 10;
 };
 
 template <class data_T, class index_T, typename CONFIG_T>
-void gather(data_T input[CONFIG_T::n_in], index_T index[CONFIG_T::n_indices], data_T output[CONFIG_T::n_indices]) {
+void gather_1d(data_T input[CONFIG_T::n_in], index_T index[CONFIG_T::n_indices], data_T output[CONFIG_T::n_indices]) {
 
     #pragma HLS PIPELINE
 
     for (int i = 0; i < CONFIG_T::n_indices; i++) {
         output[i] = input[index[i]];
+    }
+}
+
+struct gather_2d_config {
+    static const unsigned n_in_0 = 20;
+    static const unsigned n_in_1 = 20;
+    static const unsigned n_indices = 10;
+};
+
+template <class data_T, class index_T, typename CONFIG_T>
+void gather_2d(data_T input[CONFIG_T::n_in_0 * CONFIG_T::n_in_1], index_T index[CONFIG_T::n_indices],
+               data_T output[CONFIG_T::n_indices * CONFIG_T::n_in_1]) {
+
+    #pragma HLS PIPELINE
+
+    for (int i = 0; i < CONFIG_T::n_indices; i++) {
+        for (int j = 0; j < CONFIG_T::n_in_1; j++) {
+            output[i * CONFIG_T::n_in_1 + j] = input[index[i] * CONFIG_T::n_in_1 + j];
+        }
+    }
+}
+
+struct gather_3d_config {
+    static const unsigned n_in_0 = 10;
+    static const unsigned n_in_1 = 10;
+    static const unsigned n_in_2 = 10;
+    static const unsigned n_indices = 10;
+};
+
+template <class data_T, class index_T, typename CONFIG_T>
+void gather_3d(data_T input[CONFIG_T::n_in_0 * CONFIG_T::n_in_1 * CONFIG_T::n_in_2], index_T index[CONFIG_T::n_indices],
+               data_T output[CONFIG_T::n_indices * CONFIG_T::n_in_1 * CONFIG_T::n_in_2]) {
+
+#pragma HLS PIPELINE
+
+    for (int i = 0; i < CONFIG_T::n_indices; i++) {
+        for (int j = 0; j < CONFIG_T::n_in_1; j++) {
+            for (int k = 0; k < CONFIG_T::n_in_2; k++) {
+                output[i * CONFIG_T::n_in_1 * CONFIG_T::n_in_2 + j * CONFIG_T::n_in_2 + k] = input[index[i] * CONFIG_T::n_in_1 * CONFIG_T::n_in_2 + j * CONFIG_T::n_in_2 + k];
+            }
+        }
     }
 }
 
